5ff65329d3f84e74829a98524879a7a290029a2d
LianRicardo <idv16c.lcamejo@uartesdigitales.edu.mx>
2018-10-03 13:00:27 -0500

    Esqueletos de clases CBoid y CGameObject

--- AI_SteeringBehaviors/CBoid.h
+++ AI_SteeringBehaviors/CBoid.h
@@ -7,5 +7,15 @@ class CBoid : public CGameObject
 public:
 	CBoid();
 	~CBoid();
+private:
+	CVector2 direccion;
+	CVector2 seek();
+	CVector2 flee();
+	CVector2 pursue();
+	CVector2 evade();
+	CVector2 wander_random();
+	CVector2 wander_dir();
+	CVector2 follow_path();
+	CVector2 patrol();
 };
 
--- AI_SteeringBehaviors/CGameObject.h
+++ AI_SteeringBehaviors/CGameObject.h
@@ -1,7 +1,10 @@
 #pragma once
+#include "CVector2.h"
+
 class CGameObject
 {
 public:
+	CVector2 pos;
 	CGameObject();
 	virtual ~CGameObject();
 };
